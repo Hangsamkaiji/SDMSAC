d3fb318977b88fe50ff1895c2d74450d687ee51f
Hangsam <102114166+Hangsamkaiji@users.noreply.github.com>
2022-03-22 10:46:53 +1100

    Add files via upload

---
+++ task2/task2/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace task2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDebug = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblDebug
+            // 
+            this.lblDebug.AutoSize = true;
+            this.lblDebug.Location = new System.Drawing.Point(727, 29);
+            this.lblDebug.Name = "lblDebug";
+            this.lblDebug.Size = new System.Drawing.Size(72, 15);
+            this.lblDebug.TabIndex = 1;
+            this.lblDebug.Text = "debug Label";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(71, 60);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 89);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(1178, 717);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1219, 830);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblDebug);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblDebug;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ task2/task2/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task2
+{
+    public partial class Form1 : Form
+    {
+        public float totalProfit = 0;
+        public string salePrice;
+        public float purchasePrice;
+
+        string filePath = @"C:\Users\Hangs\OneDrive\Desktop\SDM Practise SAC\task2\profitCSV.txt";
+        public List<string> lines = new List<string>();
+
+
+        public Form1()
+        {
+
+
+            InitializeComponent();
+
+            lines = File.ReadAllLines(filePath).ToList();
+            dataGridView1.ColumnCount = 8;
+
+
+            foreach (var line in lines)
+            {
+                float profit = 0;
+                List<string> fields = line.Split(',').ToList();
+                salePrice = fields[5];
+                purchasePrice = float.Parse(fields[3]);
+                if (float.TryParse(salePrice, out float _salePrice))
+                {
+                    profit = _salePrice - purchasePrice;
+
+                }
+                else
+                {
+                    profit = (purchasePrice) * -1;
+
+
+                }
+                
+                fields.Add(profit.ToString());
+                totalProfit = totalProfit + profit;
+                dataGridView1.Rows.Add(fields.ToArray());
+
+
+
+            }
+            dataGridView1.Rows.Add("", "", "", "", "","","Total Profit", "$" + totalProfit);
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+           
+
+
+        }
+
+
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+    }
+}
+
